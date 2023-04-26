e08b2919f15a61f1b73ddac5554ae4a9bfcc0fd6
Alexander Swensen <alex.swensen@gmail.com>
2023-04-25 20:35:18 -0500

    update where we get env variables

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,10 @@
 import { NODE_ENV } from '$env/static/private';
+import { PUBLIC_SENTRY_DSN } from '$env/static/public';
 import * as SentryNode from '@sentry/node';
 import '@sentry/tracing';
 import type { HandleServerError } from '@sveltejs/kit';
 
-const { PUBLIC_SENTRY_DSN, SENTRY_ORG, SENTRY_PROJECT } = process.env;
-
-const sentryEnabled = PUBLIC_SENTRY_DSN && SENTRY_ORG && SENTRY_PROJECT;
+const sentryEnabled = PUBLIC_SENTRY_DSN;
 
 if (sentryEnabled) {
 	SentryNode.init({
