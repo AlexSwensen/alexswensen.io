f3eb403fe2013208ecf89c97f5ee53ba491e033b
Alexander Swensen <alex.swensen@gmail.com>
2023-09-29 16:03:27 -0500

    add a working vitest

--- src/lib/components/Nav/Nav.test.ts
+++ src/lib/components/Nav/Nav.test.ts
@@ -5,7 +5,7 @@ import Nav from './Nav.svelte';
 describe('Nav', () => {
 	test('that nav is rendered', () => {
 		const { getByLabelText, getByText, component } = render(Nav, {});
-		const nav = getByLabelText('alexswensen.io');
-		expect(nav).toBeInTheDocument();
+		const nav = getByText('alexswensen.io');
+		expect(nav).toBeDefined();
 	});
 });
