9958aad483aa00c6c9559754989f7c85618ef5f4
Alexander Swensen <alex.swensen@gmail.com>
2023-10-19 11:01:12 -0500

    add redirect map and older posts

--- src/lib/services/redirects.ts
+++ src/lib/services/redirects.ts
@@ -1,4 +1,8 @@
 export const redirectMap = new Map([
-	['/old-page', '/blog/new-page'],
-	['/another-old-page', '/blog/another-new-page']
+	['/2020-12-09_popos-on-2011-macbook-pro', '/blog/2020-12-09_popos-on-2011-macbook-pro'],
+	['/2020-12-08_android-to-iphone/', '/blog/2020-12-08_android-to-iphone'],
+	['/regarding-hacktoberfest/', '/blog/regarding-hacktoberfest'],
+	['/vscode-python3-intellisense', '/blog/vscode-python3-intellisense'],
+	['/wemos-esp32-notes', '/blog/wemos-esp32-notes'],
+	['/writing-e2e-tests-with-protractor', '/blog/writing-e2e-tests-with-protractor'],
 ]);
