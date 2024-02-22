776ab2055e872dbfd0c82b1597a9c462fcf0eeea
Alexander Swensen <alex.swensen@gmail.com>
2024-02-22 16:52:42 -0600

    mobile and desktop color theme toggles

--- src/lib/components/Nav/Nav.svelte
+++ src/lib/components/Nav/Nav.svelte
@@ -1,25 +1,29 @@
 <script lang="ts">
-	import {page} from '$app/stores';
-	import { NavBrand, NavHamburger, NavLi, NavUl, Navbar } from 'flowbite-svelte';
+	import { page } from '$app/stores';
+	import { DarkMode, NavBrand, NavHamburger, NavLi, NavUl, Navbar } from 'flowbite-svelte';
+	import NavContainer from 'flowbite-svelte/NavContainer.svelte';
 	$: activeUrl = $page.url.pathname;
 </script>
 
 <Navbar>
-	<NavBrand href="/">
-		<!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
-		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-			>alexswensen.io</span
-		>
-	</NavBrand>
-	<NavHamburger />
-	<NavUl activeUrl={activeUrl}>
-		<NavLi href="/">Home</NavLi>
-		<NavLi href="/about">About</NavLi>
-		<NavLi href="/blog">Blog</NavLi>
-		<NavLi href="/resume">Resume</NavLi>
-		<!-- <NavLi href="/contact">Contact</NavLi> -->
-	</NavUl>
-	
+	<NavContainer class="">
+		<DarkMode btnClass="block md:hidden mr-3" />
+		<NavBrand href="/" class="mr-auto md:mr-0">
+			<!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+				>alexswensen.io</span
+			>
+		</NavBrand>
+		<NavHamburger />
+		<NavUl {activeUrl} class="ml-auto">
+			<NavLi href="/">Home</NavLi>
+			<NavLi href="/about">About</NavLi>
+			<NavLi href="/blog">Blog</NavLi>
+			<NavLi href="/resume">Resume</NavLi>
+			<!-- <NavLi href="/contact">Contact</NavLi> -->
+		</NavUl>
+		<DarkMode btnClass="ml-3 hidden md:block" />
+	</NavContainer>
 </Navbar>
 <!-- <div class="navbar bg-base-200">
 	<div class="flex-1">
