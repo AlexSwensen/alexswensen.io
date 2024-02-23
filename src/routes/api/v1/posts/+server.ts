5ce3395af72ef205667cf3a9ead9678fc84b1ec5
Alexander Swensen <alex.swensen@gmail.com>
2024-02-23 17:44:10 -0600

    return posts sorted by date

--- src/routes/api/v1/posts/+server.ts
+++ src/routes/api/v1/posts/+server.ts
@@ -1,7 +1,12 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
+import { compareDesc } from 'date-fns';
 // @ts-ignore-next
 const posts = import.meta.compileTime('../../../../lib/static-content/get-posts.ts');
 
 export const GET: RequestHandler = async () => {
-	return json(posts);
+	// sort posts by date
+	const sortedPosts = posts.posts.sort((a: any, b: any) =>
+		compareDesc(new Date(a.date), new Date(b.date))
+	);
+	return json({ posts: sortedPosts });
 };
