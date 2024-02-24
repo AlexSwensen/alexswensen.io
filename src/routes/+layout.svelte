cfa7a6aec700228877eaa922e60e59eb5f61dff7
Alexander Swensen <alex.swensen@gmail.com>
2024-02-24 12:20:00 -0600

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import '../app.scss';
 	import Nav from '../lib/components/Nav/Nav.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
