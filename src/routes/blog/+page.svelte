7bdda82c84c69192307ed2772aa6878e47faae8a
Alexander Swensen <alex.swensen@gmail.com>
2024-02-27 15:10:59 -0600

    bit of cleanup

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -24,19 +24,11 @@
 	<meta name="bingbot" content="index, follow" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<meta name="theme-color" content="#000000" />
-	<meta name="apple-mobile-web-app-capable" content="yes" />
-	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
-	<meta name="apple-mobile-web-app-title" content="alexswensen.io" />
-	<meta name="application-name" content="alexswensen.io" />
-	<meta name="msapplication-TileColor" content="#000000" />
-	<meta name="msapplication-TileImage" content="/images/ms-icon-144x144.png" />
-	<meta name="msapplication-config" content="/images/browserconfig.xml" />
-	<meta name="theme-color" content="#000000" />
 	<link rel="apple-touch-icon" sizes="57x57" href="/images" />
-	<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
-	<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
 	<link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#5bbad5" />
-	<link rel="shortcut icon" href="/images/favicon.ico" />
+	<link rel="shortcut icon" href="/favicon.png" />
 </svelte:head>
 
 <Heading size="2xl">Alex Swensen's Blog</Heading>
