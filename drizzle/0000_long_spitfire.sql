bc1458a5fd1d974dfe0ebca73a742040e91a6341
Alexander Swensen <alex.swensen@gmail.com>
2024-03-14 15:29:03 -0500

    start of query support

--- drizzle/0000_long_spitfire.sql
+++ drizzle/0000_long_spitfire.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS "users" (
 	"name" text NOT NULL,
 	"email" text NOT NULL,
 	"hashed_password" text NOT NULL,
-	"created_at" text NOT NULL,
-	"updated_at" text NOT NULL,
+	"created_at" text DEFAULT now() NOT NULL,
+	"updated_at" text DEFAULT now() NOT NULL,
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
--- src/lib/db/schema/users.ts
+++ src/lib/db/schema/users.ts
@@ -1,12 +1,17 @@
 // src/lib/db/schema.ts
 
 import { pgTable, serial, text } from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
 
 export const users = pgTable('users', {
 	id: serial('id').primaryKey(),
 	name: text('name').notNull(),
 	email: text('email').notNull().unique(),
 	hashedPassword: text('hashed_password').notNull(),
-	created_at: text('created_at').notNull(),
-	updated_at: text('updated_at').notNull()
+	created_at: text('created_at')
+		.notNull()
+		.default(sql`now()`),
+	updated_at: text('updated_at')
+		.notNull()
+		.default(sql`now()`)
 });
