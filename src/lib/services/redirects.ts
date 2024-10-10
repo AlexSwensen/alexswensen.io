ac2046bcb02e7b37985d37b242c634aa5f1c34fd
Alexander Swensen <alex.swensen@gmail.com>
2024-10-10 14:53:49 -0500

    update redirect from a very old blog post refd on github

--- src/lib/services/redirects.ts
+++ src/lib/services/redirects.ts
@@ -6,6 +6,7 @@ export const redirectMap = new Map([
 	['/2020-12-08_android-to-iphone/', '/blog/2020-12-08_android-to-iphone'],
 	['/regarding-hacktoberfest/', '/blog/regarding-hacktoberfest'],
 	['/vscode-python3-intellisense/', '/blog/vscode-python3-intellisense'],
+	['/2019-12-06-vscode-python3-intellisense/', '/blog/vscode-python3-intellisense'],
 	['/wemos-esp32-notes/', '/blog/wemos-esp32-notes'],
 	['/writing-e2e-tests-with-protractor/', '/blog/writing-e2e-tests-with-protractor']
 ]);
