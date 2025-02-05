f81ecd30578d8feee5e60dde326c66daccd7abce
Alexander Swensen <alex.swensen@gmail.com>
2025-02-05 15:17:03 -0600

    update to always show the current year

--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -1,11 +1,13 @@
 <script>
 	import { Footer, FooterCopyright, FooterLinkGroup, FooterLink } from 'flowbite-svelte';
+
+	const currentYear = new Date().getFullYear();
 </script>
 
 <Footer>
 	<hr class="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
 	<div class="sm:flex sm:items-center sm:justify-between">
-		<FooterCopyright href="/" by="Alexander Swensen" year={2024} />
+		<FooterCopyright href="/" by="Alexander Swensen" year={currentYear} />
 		<FooterLinkGroup
 			ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
 		>
