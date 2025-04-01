4f599a8774fb42fb98471fd27cd8e4db3498be10
Alexander Swensen <alex.swensen@gmail.com>
2025-03-31 19:37:17 -0500

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -27,6 +27,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'daily' as const,
       priority: 0.8,
     },
+    {
+      url: `${baseUrl}/resume`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly' as const,
+      priority: 1,
+    },
     // Add other static pages here
   ];
 
