cd19f93e73e68f38facda1e04fce154b86a354e5
Alexander Swensen <alex.swensen@gmail.com>
2025-10-07 09:32:49 -0500

    better dynamic schema layout

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,13 +2,13 @@ import 'server-only';
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
-import { users } from './schema/users';
+import * as schema from './schema';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-export const db = drizzle({ client: pool, schema: { users }, casing: 'snake_case' });
+export const db = drizzle({ client: pool, schema, casing: 'snake_case' });
 
 // const myUsers = db.select().from(users).where(eq(users.id, 'some-uuid'));
 // myUsers.toSQL().sql; // Check that this works without errors
